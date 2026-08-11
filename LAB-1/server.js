9312fb0263e34b6b11eb0a6c591a43edaad38a91
MeegadaVamsidhar <meegadavamsi76@gmail.com>
2026-08-11 09:21:00 +0530

    Move project files into LAB-1

--- LAB-1/server.js
+++ LAB-1/server.js
@@ -4,7 +4,7 @@ const Database = require('better-sqlite3');
 const bcrypt = require('bcryptjs');
 const session = require('express-session');
 
-const dbPath = path.join(__dirname, '..', 'mydb.db');
+const dbPath = path.join(__dirname, 'mydb.db');
 const db = new Database(dbPath);
 
 const userColumns = db.prepare("PRAGMA table_info(users)").all().map((column) => column.name);
